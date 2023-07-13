aa6e16a408c56a7c57e1fa20edb370bfb0012347
Sarah S <stms15@mun.ca>
2023-07-13 20:20:51 -0230

    started listening and added index and notes pages

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.static("public"));
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
